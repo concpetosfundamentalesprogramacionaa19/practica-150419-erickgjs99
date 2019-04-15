0f9ffbe084db4b6401c7326a476fb5b4adb9a4de
erickgjs99 <erickjs99lol@gmail.com>
2019-04-15 16:50:52 -0500

    trabajo en clase presentacion 2

---
+++ proyectos python/presentacion2.py
@@ -0,0 +1,9 @@
+"""
+Este es mi tercer ejemplo
+en el lenguaje python
+@erickgjs99
+"""
+nombre = input("Por favor ingrese su nombre:\n")
+apellido = input("Por favor ingrese su apellido:\n")
+print("Mi nombre es: %s\n\tMi apellido es: %s" %(nombre,apellido))
+
